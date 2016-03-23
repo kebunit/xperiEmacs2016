bffe99aefbbf57bd4fda433909989e08760f09ec
dsapoetra <angga.dimassaputra@gmail.com>
2016-03-24 06:41:18 +0700

    template

--- src/main.cpp
+++ src/main.cpp
@@ -139,146 +139,175 @@ if (panjang < 10){
 
  else if (panjang >=10 && panjang <20){
    cout <<"sedang"<<endl;
-   char matrix [panjang][lebar];
+   cout<<"kecil"<<endl;
+  char matrix [panjang][lebar];
+  Singa Sin ( 1, 0, 1, rand()%100);
+  Singa Sin2 ( 2, 3, 1, rand()%100);
+  Singa Sin3 ( 3, 5, 1, rand()%100);
+
+  Serigala Ser ( 4, 3, 1, rand()%100);
+  Serigala Ser2 ( 5, 9, 1, rand()%100);
+  Serigala Ser3 ( 6, 7, 1, rand()%100);
+
+  Jerapah  J ( 7, 9, 1, rand()%100);
+  Jerapah  J2 ( 8, 6, 1, rand()%100);
+  Jerapah  J3 ( 9, 3, 1, rand()%100);
+  Kuda K ( 10, 1, 1, rand()%100);
+  Kuda K2 ( 9, 2, 1, rand()%100);
+   Kuda K3 ( 8, 3, 1, rand()%100);
+  Wortel W ( 7, 8, rand()%100);
+  Wortel W2 ( 6, 6, rand()%100);
+  Wortel W3 ( 5, 5, rand()%100);
+  Rumput R( 4, 2, rand()%100);
+  Rumput R2( 3, 1, rand()%100);
+  Rumput R3( 2, 2, rand()%100);
+  Makhluk* matrix2[panjang][lebar];
+  for(int x=0;x<panjang;x++)
+    {
+        for(int y=0;y<lebar;y++)
+        {
+            matrix2[x][y]=&Sins;
+        }
+    }
+
+  for(int x=0;x<panjang;x++)
+    {
+        for(int y=0;y<lebar;y++)
+        {
+             if (matrix2[x][y]->GetId()==-1){
+               if (x == Sin.GetPosisiX()&&y==Sin.GetPosisiY){
+                 matrix2[x][y]=&Sin;
+               }
+                 else if (x == Sin2.GetPosisiX()&&y==Sin2.GetPosisiY){
+                   matrix2[x][y]=&Sin2;
+                 }
+                else if (x == Sin3.GetPosisiX()&&y==Sin3.GetPosisiY){
+                   matrix2[x][y]=&Sin3;
+                 }
+                else if (x == Sin2.GetPosisiX()&&y==Sin2.GetPosisiY){
+                   matrix2[x][y]=&Sin2;
+                 }
+        }
+    }
+
+
+
+
     for(int x=0;x<panjang;x++)
     {
         for(int y=0;y<lebar;y++)
         {
-            matrix[x][y]='*';
+           if (matrix2[x][y]->GetId()==-1){
+             cout <<'*';
+           }
+           if (matrix2[x][y]-> GetPower()==10&matrix2[x][y]->GetId()!= -1){
+             cout<<'S';
+           }
+           else  if (matrix2[x][y]-> GetPower()==9){
+             cout<<'J';
+           }
+                     else  if (matrix2[x][y]-> GetPower()==8){
+             cout<<'s';
+           }
+                     else  if (matrix2[x][y]-> GetPower()==7){
+             cout<<'K';
+           }
+                                else  if (matrix2[x][y]-> GetPower()==6){
+             cout<<'R';
+           }
+                                else  if (matrix2[x][y]-> GetPower()==5){
+             cout<<'W';
+           }
         }
+        cout<<endl;
     }
 
-    for (int i = 0; i < 3; i++){
-      for (int y =0; y<3;y++){
-       int v1 = rand() % panjang;
-      int v2 = rand() % lebar;
-      matrix[v1][v2]='S';
-      }
-    }
-
-    for (int i = 0; i < 3; i++){
-      for (int y =0; y<3;y++){
-       int v1 = rand() % panjang;
-      int v2 = rand() % lebar;
-      matrix[v1][v2]='s';
-      }
-    }
-
-        for (int i = 0; i < 3; i++){
-      for (int y =0; y<3;y++){
-       int v1 = rand() % panjang;
-      int v2 = rand() % lebar;
-      matrix[v1][v2]='R';
-      }
-    }
-
-            for (int i = 0; i < 3; i++){
-      for (int y =0; y<3;y++){
-       int v1 = rand() % panjang;
-      int v2 = rand() % lebar;
-      matrix[v1][v2]='W';
-      }
-    }
-
-                for (int i = 0; i < 3; i++){
-      for (int y =0; y<3;y++){
-       int v1 = rand() % panjang;
-      int v2 = rand() % lebar;
-      matrix[v1][v2]='K';
-      }
-    }
-
-                    for (int i = 0; i < 3; i++){
-      for (int y =0; y<3;y++){
-       int v1 = rand() % panjang;
-      int v2 = rand() % lebar;
-      matrix[v1][v2]='J';
-      }
-    }
-
-
-    // showing the matrix on the screen
-
-    for(int x=0;x<panjang;x++)  // loop 3 times for three lines
-    {
-        for(int y=0;y<lebar;y++)  // loop for the three elements on the line
-        {
-            cout<<matrix[x][y];  // display the current element out of the array
-        }
-    cout<<endl;  // when the inner loop is done, go to a new line
-    }
 
 }
  else if (panjang >= 20){
-   cout<<"besar"<<endl;
-   char matrix [panjang][lebar];
+   cout<<"kecil"<<endl;
+  char matrix [panjang][lebar];
+  Singa Sin ( 1, 0, 1, rand()%100);
+  Singa Sin2 ( 2, 3, 1, rand()%100);
+  Singa Sin3 ( 3, 5, 1, rand()%100);
+
+  Serigala Ser ( 4, 3, 1, rand()%100);
+  Serigala Ser2 ( 5, 9, 1, rand()%100);
+  Serigala Ser3 ( 6, 7, 1, rand()%100);
+
+  Jerapah  J ( 7, 9, 1, rand()%100);
+  Jerapah  J2 ( 8, 6, 1, rand()%100);
+  Jerapah  J3 ( 9, 3, 1, rand()%100);
+  Kuda K ( 10, 1, 1, rand()%100);
+  Kuda K2 ( 9, 2, 1, rand()%100);
+   Kuda K3 ( 8, 3, 1, rand()%100);
+  Wortel W ( 7, 8, rand()%100);
+  Wortel W2 ( 6, 6, rand()%100);
+  Wortel W3 ( 5, 5, rand()%100);
+  Rumput R( 4, 2, rand()%100);
+  Rumput R2( 3, 1, rand()%100);
+  Rumput R3( 2, 2, rand()%100);
+  Makhluk* matrix2[panjang][lebar];
+  for(int x=0;x<panjang;x++)
+    {
+        for(int y=0;y<lebar;y++)
+        {
+            matrix2[x][y]=&Sins;
+        }
+    }
+
+  for(int x=0;x<panjang;x++)
+    {
+        for(int y=0;y<lebar;y++)
+        {
+             if (matrix2[x][y]->GetId()==-1){
+               if (x == Sin.GetPosisiX()&&y==Sin.GetPosisiY){
+                 matrix2[x][y]=&Sin;
+               }
+                 else if (x == Sin2.GetPosisiX()&&y==Sin2.GetPosisiY){
+                   matrix2[x][y]=&Sin2;
+                 }
+                else if (x == Sin3.GetPosisiX()&&y==Sin3.GetPosisiY){
+                   matrix2[x][y]=&Sin3;
+                 }
+                else if (x == Sin2.GetPosisiX()&&y==Sin2.GetPosisiY){
+                   matrix2[x][y]=&Sin2;
+                 }
+        }
+    }
+
+
+
+
     for(int x=0;x<panjang;x++)
     {
         for(int y=0;y<lebar;y++)
         {
-            matrix[x][y]='*';
+           if (matrix2[x][y]->GetId()==-1){
+             cout <<'*';
+           }
+           if (matrix2[x][y]-> GetPower()==10&matrix2[x][y]->GetId()!= -1){
+             cout<<'S';
+           }
+           else  if (matrix2[x][y]-> GetPower()==9){
+             cout<<'J';
+           }
+                     else  if (matrix2[x][y]-> GetPower()==8){
+             cout<<'s';
+           }
+                     else  if (matrix2[x][y]-> GetPower()==7){
+             cout<<'K';
+           }
+                                else  if (matrix2[x][y]-> GetPower()==6){
+             cout<<'R';
+           }
+                                else  if (matrix2[x][y]-> GetPower()==5){
+             cout<<'W';
+           }
         }
+        cout<<endl;
     }
 
-    for (int i = 0; i < 3; i++){
-      for (int y =0; y<3;y++){
-       int v1 = rand() % panjang;
-      int v2 = rand() % lebar;
-      matrix[v1][v2]='S';
-      }
-    }
-
-    for (int i = 0; i < 3; i++){
-      for (int y =0; y<3;y++){
-       int v1 = rand() % panjang;
-      int v2 = rand() % lebar;
-      matrix[v1][v2]='s';
-      }
-    }
-
-        for (int i = 0; i < 3; i++){
-      for (int y =0; y<3;y++){
-       int v1 = rand() % panjang;
-      int v2 = rand() % lebar;
-      matrix[v1][v2]='R';
-      }
-    }
-
-            for (int i = 0; i < 3; i++){
-      for (int y =0; y<3;y++){
-       int v1 = rand() % panjang;
-      int v2 = rand() % lebar;
-      matrix[v1][v2]='W';
-      }
-    }
-
-                for (int i = 0; i < 3; i++){
-      for (int y =0; y<3;y++){
-       int v1 = rand() % panjang;
-      int v2 = rand() % lebar;
-      matrix[v1][v2]='K';
-      }
-    }
-
-                    for (int i = 0; i < 3; i++){
-      for (int y =0; y<3;y++){
-       int v1 = rand() % panjang;
-      int v2 = rand() % lebar;
-      matrix[v1][v2]='J';
-      }
-    }
-
-
-    // showing the matrix on the screen
-
-    for(int x=0;x<panjang;x++)  // loop 3 times for three lines
-    {
-        for(int y=0;y<lebar;y++)  // loop for the three elements on the line
-        {
-            cout<<matrix[x][y];  // display the current element out of the array
-        }
-    cout<<endl;  // when the inner loop is done, go to a new line
-    }
 
    }
 
